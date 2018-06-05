bbe77ee03a2f09570cbcb5bed51c82736c03ce56
Brendan Sapience <brendan.sapience@AAI.AASPL-BRD.COM>
2018-06-05 12:43:49 -0700

    initial commit

---
+++ CustomAppMetaBot/CustomAppTests/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyCustomAppLibrary;
+
+namespace CustomAppTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsefulMethods methods = new UsefulMethods();
+            String Res = methods.GetGreeting("Bren");
+            Console.WriteLine("Debug:" + Res);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CustomAppMetaBot/MyCustomAppLibrary/UsefulMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCustomAppLibrary
+{
+    public class UsefulMethods
+    {
+        public String GetGreeting(String Name)
+        {
+            return "Hi there, " + Name + "!";
+        }
+    }
+}
